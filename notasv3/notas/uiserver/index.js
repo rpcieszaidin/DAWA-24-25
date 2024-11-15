b9e6f182d87edc6472cc6c4518083aa0da36e0d9
rpcieszaidin <114917189+rpcieszaidin@users.noreply.github.com>
2024-11-15 10:16:55 +0100

    revisión

--- notasv3/notas/uiserver/index.js
+++ notasv3/notas/uiserver/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import { model1 } from './interfaces/model_1.js';
+import { notes } from './interfaces/notes.js';
 
 const appUI = new express();
 appUI.use(cors());
 
-appUI.get('/', (req, res) => {
+appUI.get('/ui', (req, res) => {
     res.send(model1);
+})
+
+appUI.get('/notes', (req, res) => {
+    res.send(notes);
 })
 
 appUI.listen(3000, ()=>{
--- notasv3/notas/uiserver/interfaces/notes.js
+++ notasv3/notas/uiserver/interfaces/notes.js
@@ -1,6 +1,20 @@
-export const notes = {
-    notes: [
-        
-    ],
+export const monton = {
+    monton: [
+        {
+            id:1,
+            mes:10,
+            ano:200,
+            notas:[
+              {
+               tipo:'normal',
+               contenido:'hola',
+               id:23,
+                
+              }  
+
+            ]
+
+        }
+    ]
     
 }
