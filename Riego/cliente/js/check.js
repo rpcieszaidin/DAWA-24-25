08899ce49413c64ab6edc7e0c12652abf1e91980
rpcieszaidin <rauljimenez@ieszaidinvergeles.org>
2024-12-13 08:25:14 +0100

    Prueba2. Evaluable.

---
+++ Riego/cliente/js/check.js
@@ -0,0 +1,33 @@
+export class Check {
+    constructor(parent,client) {
+        this.parent = parent;
+        this.client = client;
+        this.states = [];
+    }
+
+    changeValue(name, value) {
+        const data = this.states.find((item) => item.name == name);
+        // hacer algo más.
+    }
+
+    addCheck(name) {
+        this.states.push({
+            name : name,
+            state : false
+        })
+        const check = document.createElement("label");
+        check.classList.add("form-switch");
+        this.parent.appendChild(check);
+        const input = document.createElement("input");
+        input.setAttribute('type', 'checkbox');
+        check.appendChild(input);
+        check.appendChild(document.createElement("i"));
+        const span = document.createElement('span');
+        const text = document.createTextNode('OFF');
+        span.appendChild(text);
+        check.appendChild(span);
+        input.addEventListener('change', (event)=> {
+            this.changeValue(name, event.target.checked);
+        })
+    }
+}
